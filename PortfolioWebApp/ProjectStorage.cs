503c04aa5b667aee0e2e22be27621a85437d822c
Mikhail <bogdi.zp@gmail.com>
2021-09-30 17:57:43 +0300

    add project PortfolioWebApp

---
+++ PortfolioWebApp/ProjectStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioWebApp
+{
+    public static class ProjectStorage
+    {
+        public static List<Project> Projects { get; private set; } = new List<Project>()
+    }
+}
